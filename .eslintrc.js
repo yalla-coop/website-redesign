fa291fd726d3aae1587ce2b728e773384843dc57
Imad Shatali <amoodaa@gmail.com>
2020-03-02 12:45:58 +0200

    did some vscode workplace config for formatting
    using code.fixAll for javascript files
    using prettier for everything else

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
-    extends: ['airbnb', 'airbnb/hooks', 'prettier', 'prettier/react'],
-    plugins: ['react', 'prettier'],
-    // babel-eslint parser is used to support experimental features not supported in ESLint itself yet
-    parser: 'babel-eslint',
-    parserOptions: {
-      ecmaVersion: 8,
-      ecmaFeatures: {
-        impliedStrict: true, //enable global strict mode (if ecmaVersion is 5 or greater)
+  extends: ['airbnb', 'airbnb/hooks', 'prettier', 'prettier/react'],
+  plugins: ['react', 'prettier'],
+  // babel-eslint parser is used to support experimental features not supported in ESLint itself yet
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      impliedStrict: true, //enable global strict mode (if ecmaVersion is 5 or greater)
+    },
+  },
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'arrow-body-style': ['error', 'as-needed'],
+    // disables the windows/unix linebreak checks.
+    'linebreak-style': [0, 'error', 'windows'],
+    //  allow .js extensions for JSX.
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
       },
-    },
-    root: true,
-    env: {
-      browser: true,
-      node: true,
-      jest: true,
-    },
-    rules: {
-      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-      'arrow-body-style': ['error', 'as-needed'],
-      // disables the windows/unix linebreak checks.
-      'linebreak-style': [0, 'error', 'windows'],
-      //  allow .js extensions for JSX.
-      'react/jsx-filename-extension': [
-        1,
-        {
-          extensions: ['.js', '.jsx'],
-        },
-      ],
-      // configure the prettier plugin
-      'prettier/prettier': [
-        'error',
-        {
-          trailingComma: 'all',
-          singleQuote: true,
-        },
-      ],
-    },
-  };
+    ],
+    // configure the prettier plugin
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+  },
+};
