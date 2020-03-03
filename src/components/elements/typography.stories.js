bb513e55c5cd8b0c124f35881bd48b35c14b5ada
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-03 21:16:50 +0200

    Add storybook for typography components > #19

---
+++ src/components/elements/typography.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { H1, H2, H3, H4, Link, P } from './typography'
+
+export default {
+  title: 'Design System/Typography',
+}
+
+export const h1 = () => <H1>Heading 1</H1>
+export const h2 = () => <H2>Heading 2</H2>
+export const h3 = () => <H3>Heading 3</H3>
+export const h4 = () => <H4>Heading 4</H4>
+
+export const link = () => <Link to="/">Click Here</Link>
+
+export const paragraph = () => <P>This is a paragraph</P>
