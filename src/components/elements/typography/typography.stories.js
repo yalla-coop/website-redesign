5ed0f2d248142ef08076a25919d91306cb12aa21
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-04 13:58:40 +0200

    chore: add new caps headings

--- src/components/elements/typography/typography.stories.js
+++ src/components/elements/typography/typography.stories.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import { H1, H2, H3, H4, Link, P } from './typography'
-import { size } from '../../../utils/typography'
+import {
+  H1,
+  H2,
+  H3,
+  H4,
+  CapsHeadingLarge,
+  CapsHeadingMedium,
+  CapsHeadingSmall,
+  Link,
+  P,
+} from './typography'
+import { size, letterSpacing } from '../../../utils'
 
 export default {
   title: 'Design System/Typography',
@@ -10,6 +20,30 @@ export const h1 = () => <H1>Heading 1</H1>
 export const h2 = () => <H2>Heading 2</H2>
 export const h3 = () => <H3>Heading 3</H3>
 export const h4 = () => <H4>Heading 4</H4>
+
+export const CapsHeadingLargeExtraBold = () => (
+  <CapsHeadingLarge w={800}>caps heading large</CapsHeadingLarge>
+)
+
+export const CapsHeadingLargeBold = () => (
+  <CapsHeadingLarge>caps heading large</CapsHeadingLarge>
+)
+
+export const CapsHeadingLargeMedium = () => (
+  <CapsHeadingLarge w={500}>caps heading large</CapsHeadingLarge>
+)
+
+export const CapsHeadingMediumRegular = () => (
+  <CapsHeadingMedium>caps heading medium</CapsHeadingMedium>
+)
+
+export const CapsHeadingSmallRegular = () => (
+  <CapsHeadingSmall>caps heading small</CapsHeadingSmall>
+)
+
+export const CapsHeadingSmallLetterSpacing = () => (
+  <CapsHeadingSmall l={letterSpacing.huge}>caps heading small</CapsHeadingSmall>
+)
 
 export const link = () => <Link to="/">Click Here</Link>
 
