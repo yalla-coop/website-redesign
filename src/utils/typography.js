a60329f492a4208d49081a68a14d47e37541c508
Imad Shatali <amoodaa@gmail.com>
2020-03-04 15:32:44 +0200

    build: added paragraphs

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,7 @@
 import './font.css'
 
 export const font = `font-family: 'Karu', sans-serif`
+
 export const size = {
   xxs: '1.2rem',
   xs: '1.8rem',
@@ -8,4 +9,12 @@ export const size = {
   md: '3rem',
   lg: '4rem',
   xl: '6rem',
+}
+
+export const psize = {
+  xs: '1.2rem',
+  s: '1.4rem',
+  m: '1.8rem',
+  l: '2rem',
+  xl: '2.2rem',
 }
