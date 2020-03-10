0f630abf3926275e07f52cd1068c94af0c934bb5
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-11 00:06:36 +0200

    build: work card component > #22

---
+++ src/components/common/WorkCard/index.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import { WorkWrapper, TextWrapper } from './styles'
+import { CapsHeadingSmall, H4 } from '../../elements'
+
+import Image from '../../image'
+
+const WorkCard = ({ smallHeading, bigHeading, fileName, altText }) => (
+  <WorkWrapper>
+    <Image fileName={fileName} altText={altText} />
+    <TextWrapper>
+      <CapsHeadingSmall>{smallHeading}</CapsHeadingSmall>
+      <H4>{bigHeading}</H4>
+    </TextWrapper>
+  </WorkWrapper>
+)
+
+WorkCard.propTypes = {
+  bigHeading: PropTypes.string.isRequired,
+  fileName: PropTypes.string.isRequired,
+  smallHeading: PropTypes.string.isRequired,
+  altText: PropTypes.string.isRequired,
+}
+
+export default WorkCard
---
+++ src/components/common/WorkCard/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+import { colors } from '../../../utils'
+
+export const WorkWrapper = styled.div`
+  padding: 3.5rem;
+  background-color: ${colors.primary};
+  width: 100%;
+  height: 710;
+  color: ${colors.white};
+`
+
+export const TextWrapper = styled.div`
+  width: 90%;
+  margin: 2.5rem 4rem 0 1.6rem;
+`
