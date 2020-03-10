5e2427279ec5c54c9a4872a06db328be8822ca1d
Imad Shatali <amoodaa@gmail.com>
2020-03-10 03:35:35 +0200

    build: button with alot of props

---
+++ src/components/elements/buttons/index.stories.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { withKnobs, text, boolean, color } from '@storybook/addon-knobs'
+import Button from './index'
+
+export default {
+  title: 'Button',
+  decorators: [withKnobs],
+}
+
+export const PrimaryButton = () => (
+  <Button primary title={text('title', 'GET IN TOUCH')} />
+)
+
+export const CustomButton = () => (
+  <Button
+    primary
+    title={text('title', 'GET IN TOUCH')}
+    hasArrow={boolean('hasArrow', true)}
+    textColor={color('textColor')}
+    backgroundColor={color('backgroundColor', 'rgba(0,0,0,1)')}
+    capitalize={boolean('capitalize', true)}
+  />
+)
