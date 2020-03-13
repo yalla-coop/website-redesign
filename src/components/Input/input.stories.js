420b73eee9195ca8b0a66936ddcc1fa6d8a19814
fadeomar <fadeomar2015@gmail.com>
2020-03-13 23:12:56 +0200

    style: check the textarea and the  input

--- src/components/Input/input.stories.js
+++ src/components/Input/input.stories.js
@@ -5,4 +5,33 @@ export default {
   title: 'Input',
 }
 
-export const CustomInput = () => <Input />
+export const CustomInput = () => (
+  <>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Input
+        name="username"
+        label="full name"
+        placeholder="add you dull name"
+        onChange={() => null}
+      />
+      <Input
+        name="password"
+        label="company"
+        placeholder="add your company name"
+        onChange={() => null}
+      />
+    </div>
+    <Input
+      big
+      name="text area"
+      label="message"
+      placeholder="let is know your idea"
+      onChange={() => null}
+    />
+  </>
+)
