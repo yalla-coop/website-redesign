c651ce9d619304465ec0d18cebcafd891e82fde9
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-14 21:38:28 +0200

    style(components): quote component

---
+++ src/components/common/Quote/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+import { H3, XLP, CapsHeadingLarge, CapsHeadingMedium } from '../../elements'
+
+export const QuoteWrapper = styled.div`
+  width: 80%;
+`
+
+export const H3Modifier = styled(H3)`
+  color: #000000;
+  margin-bottom: 50px; /* the space between it and the paragraph */
+`
+
+export const QuoteIcon = styled.span`
+  font-size: 100px;
+  color: #c73e46;
+  font-weight: 700;
+
+  /* and these spacings: */
+  margin: 0 20px 15px 0;
+`
+
+export const HeadlineWrapper = styled.div`
+  display: flex;
+`
+
+export const BodyWrapper = styled.div`
+  transform: translateX(66px);
+`
+
+export const XLPModifier = styled(XLP)`
+  color: #858585;
+  margin-bottom: 8px;
+`
+
+export const CapsHeadingLargeModifier = styled(CapsHeadingLarge)`
+  color: #000000;
+  margin-bottom: 5px;
+`
+
+export const CapsHeadingMediumModifier = styled(CapsHeadingMedium)`
+  color: #c4c4c4;
+`
