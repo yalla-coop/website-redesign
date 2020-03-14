0a7550f5dfac77cb34892214df84e5663f658c57
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-14 22:17:57 +0200

    style(components): write styles for story card

---
+++ src/components/common/StoryCard/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import { colors } from '../../../utils'
+
+export const StoryCardWrapper = styled.div`
+  background-color: ${colors.white};
+  width: 100%;
+  color: ${colors.primary};
+`
+
+export const TextWrapper = styled.div`
+  padding: 3rem 7rem 1rem 1rem;
+`
