c3de1fb67e2da47112f5c057c0ead8d86d8f1d50
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-17 09:51:50 +0200

    test: made storybook for each one

---
+++ src/components/elements/Accent/index.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ExtraSmall, Small, Medium, Large, ExtraLarge } from './index'
+
+export default {
+  title: 'Design System/Accent',
+}
+
+export const extraSmall = () => <ExtraSmall />
+export const small = () => <Small />
+export const medium = () => <Medium />
+export const large = () => <Large />
+export const extraLarge = () => <ExtraLarge />
