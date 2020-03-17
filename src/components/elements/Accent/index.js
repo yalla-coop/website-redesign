94c8e2149a7bc9952cdcd80014ee9e52d7bef7bf
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-17 09:50:54 +0200

    build(components): lines accent

---
+++ src/components/elements/Accent/index.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+import { colors } from '../../../utils'
+
+export const ExtraSmall = styled.div`
+  display: inline-block;
+  height: 4.2rem;
+  width: 4px;
+  background-color: ${colors.accentLight};
+`
+
+export const Small = styled.div`
+  display: inline-block;
+  height: 5.8rem;
+  width: 5px;
+  background-color: ${colors.accentLight};
+`
+export const Medium = styled.div`
+  display: inline-block;
+  height: 7.1rem;
+  width: 5px;
+  background-color: ${colors.accentLight};
+`
+
+export const Large = styled.div`
+  display: inline-block;
+  height: 20rem;
+  width: 8px;
+  background-color: ${colors.accentLight};
+`
+
+export const ExtraLarge = styled.div`
+  display: inline-block;
+  height: 332px;
+  width: 10px;
+  background-color: ${colors.accentLight};
+`
