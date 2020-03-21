ea8f6dee4cdde65ab968f3438e82f9edd1740be0
fadeomar <fadeomar2015@gmail.com>
2020-03-21 19:08:22 +0200

    build: create Subtitle component

--- src/components/Subtitle/index.js
+++ src/components/Subtitle/index.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { ExtraSmall as Accent } from '../elements/Accent'
+import { ExtraSmall, Small } from '../elements/Accent'
 
-import { CapsHeadingMedium } from '../elements/typography'
+import { CapsHeadingMedium, H2 } from '../elements/typography'
 
 const SubtitleContainer = styled.div`
   box-sizing: border-box;
   margin-top: 30px;
   position: relative;
-  padding: 50px 90px;
-  background: rgba(247, 247, 247, 0.7);
+  background: transparent;
+  padding-left: 3.5rem;
+  width: 100%;
 `
 const AccentContainer = styled.div`
   position: absolute;
-  top: -21px;
-  left: 60px;
+  top: -3rem;
+  left: 0px;
 `
-const Subtitle = ({ title }) => (
-  <SubtitleContainer>
-    <CapsHeadingMedium> {title}</CapsHeadingMedium>
-    <AccentContainer>
-      <Accent />
-    </AccentContainer>
-  </SubtitleContainer>
-)
+const Subtitle = ({ title, size }) =>
+  size === 'large' ? (
+    <SubtitleContainer>
+      <H2> {title}</H2>
+      <AccentContainer>
+        <Small />
+      </AccentContainer>
+    </SubtitleContainer>
+  ) : (
+    <SubtitleContainer>
+      <CapsHeadingMedium> {title}</CapsHeadingMedium>
+      <AccentContainer>
+        <ExtraSmall />
+      </AccentContainer>
+    </SubtitleContainer>
+  )
 Subtitle.propTypes = {
   title: PropTypes.string.isRequired,
 }
