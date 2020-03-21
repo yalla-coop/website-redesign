77534e12c27637c442c806da45ed668ead642ed8
Fadi Omar <39485495+fadeomar@users.noreply.github.com>
2020-03-21 23:49:51 +0200

    subtitle component (#53)
    * build: create Subtitle component
    
    * test: add Subtitle component to storybook
    
    * build: create Subtitle component
    
    * ci: add proptype for subtitle component
    
    * refactor: add default props for subtitle component
    
    * test: add large subtitle to storybook
    
    * refactor: change subtitle to component in the storybook
    
    * refactor: change px to rem

---
+++ src/components/Subtitle/Subtitle.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Subtitle from './index'
+
+export default {
+  title: 'components',
+}
+
+export const SubtitleSample = () => <Subtitle title="hi from Subtitle" />
+export const SubtitleLarge = () => (
+  <Subtitle title="Interested in working with us?" size="large" />
+)
