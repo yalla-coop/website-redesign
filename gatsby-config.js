d8ac167ba06e6ec747c379d568425f744478787f
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-26 00:05:20 +0200

    chore: add content for case studies

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/content/posts/`,
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
