1ac6fe9eb5b72cd6eb05e0145c7ad2dc6cd0d41d
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-28 20:30:26 +0300

    style: other sections of the landing page

--- src/pages/landing.styles.js
+++ src/pages/landing.styles.js
@@ -1,6 +1,8 @@
 import BackgroundImage from 'gatsby-background-image'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 import { H2, CapsHeadingSmall } from '../components/elements'
+import { colors } from '../utils'
 
 export const ImageBackground = styled(BackgroundImage)`
   background-image: url('/images/landing-image.png') no-repeat;
@@ -61,4 +63,60 @@ export const OurApproachSection = styled.section`
   margin-top: 30.6rem;
   margin-right: 7.6rem;
   background: rgba(247, 247, 247, 0.7);
+  display: flex;
+  height: 786px;
+`
+
+export const OurApproachHeadings = styled.div`
+  flex: 3;
+  padding-right: 10rem;
+`
+
+export const HeadingWithAccent = styled.div`
+  display: flex;
+  align-items: center;
+  transform: translate(0, -50%);
+`
+
+export const LearnMoreBlock = styled.div`
+  margin-top: 20rem;
+  padding-left: 14rem;
+
+  h2 {
+    margin-bottom: 3.6rem;
+  }
+`
+export const ServicesBlock = styled.div`
+  flex: 2;
+  margin: 8rem 0 11rem 0;
+
+  div:not(:last-child) {
+    margin-bottom: 4.8rem;
+  }
+`
+
+export const WorkSection = styled.section`
+  margin-top: 11rem;
+  background: ${colors.primary};
+  /* height: 2459px; */
+  padding: 15rem 13rem 30rem 6rem;
+  position: relative;
+`
+
+export const AllWorkLink = styled(Link)`
+  display: flex;
+  margin-top: 11rem;
+  text-decoration: none;
+`
+
+export const TestimonialsSection = styled.section`
+  margin-top: 20.4rem;
+  padding: 0 30.5rem 0 11rem;
+`
+
+export const StoriesSection = styled.section`
+  margin-top: 7rem;
+  margin-bottom: 30rem;
+  padding-left: 11rem;
+  overflow: hidden;
 `
