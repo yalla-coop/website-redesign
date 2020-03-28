91ab84b1486922d56e5c56f92c41181b2ce83686
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-03-28 20:29:49 +0300

    build: other sections of landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,13 +23,27 @@ import {
   ArrowImage,
   OurStorySection,
   OurApproachSection,
+  OurApproachHeadings,
+  HeadingWithAccent,
+  LearnMoreBlock,
+  ServicesBlock,
+  WorkSection,
+  AllWorkLink,
+  TestimonialsSection,
+  StoriesSection,
 } from './landing.styles'
 import {
   Medium as AccentMedium,
+  Large as AccentLarge,
   ExtraLarge as AccentExtraLarge,
 } from '../components/elements/Accent'
 import NavBar from '../components/layout/Header/Nav'
 import ArrowDown from '../../static/svg/arrow-down.svg'
+import Arrow from '../../static/svg/arrow'
+
+import { WorkCard, Quote, StoryCard } from '../components/common'
+
+import { colors } from '../utils'
 
 const IndexPage = () => {
   const { image } = useStaticQuery(graphql`
@@ -95,27 +109,12 @@ const IndexPage = () => {
           />
         </div>
       </OurStorySection>
-      <OurApproachSection
-        css={css`
-          display: flex;
-          justify-content: space-around;
-        `}
-      >
-        <div
-          css={css`
-            flex: 3;
-            padding-right: 10rem;
-          `}
-        >
-          <div
-            css={css`
-              display: flex;
-              align-items: center;
-              transform: translate(0, -50%);
-            `}
-          >
+      <OurApproachSection>
+        <OurApproachHeadings>
+          <HeadingWithAccent>
             <H1
               css={css`
+                color: ${colors.primary};
                 transform: rotate(-90deg);
               `}
             >
@@ -126,18 +125,13 @@ const IndexPage = () => {
                 transform: translate(-10rem, 30%);
               `}
             />
-          </div>
-          <div
-            css={css`
-              margin: 20.7rem 0 8rem 0;
-              padding: 0 11.2rem 0 18rem;
-
-              h2 {
-                margin-bottom: 3.6rem;
-              }
-            `}
-          >
-            <H2>
+          </HeadingWithAccent>
+          <LearnMoreBlock>
+            <H2
+              css={css`
+                color: ${colors.primary};
+              `}
+            >
               A user-centred approach is at the heart of everything we do.
             </H2>
             <Button
@@ -147,21 +141,18 @@ const IndexPage = () => {
               capitalize
               primary
             />
-          </div>
-        </div>
-        <div
-          css={css`
-            flex: 1;
-            margin: 8rem 0 11rem 0;
-            /* padding-right: 10rem; */
-
-            div:not(:last-child) {
-              margin-bottom: 4.8rem;
-            }
-          `}
-        >
+          </LearnMoreBlock>
+        </OurApproachHeadings>
+        <ServicesBlock>
           <div>
-            <CapsHeadingLarge>DISCOVERY / STRATEGY</CapsHeadingLarge>
+            <CapsHeadingLarge
+              css={css`
+                color: ${colors.primary};
+              `}
+              w={800}
+            >
+              DISCOVERY / STRATEGY
+            </CapsHeadingLarge>
             <LP>
               Strategy
               <br /> Discovery & Research
@@ -172,7 +163,14 @@ const IndexPage = () => {
             </LP>
           </div>
           <div>
-            <CapsHeadingLarge>Design</CapsHeadingLarge>
+            <CapsHeadingLarge
+              css={css`
+                color: ${colors.primary};
+              `}
+              w={800}
+            >
+              Design
+            </CapsHeadingLarge>
             <LP>
               UI/UX Design
               <br /> Stylescapes
@@ -182,7 +180,14 @@ const IndexPage = () => {
             </LP>
           </div>
           <div>
-            <CapsHeadingLarge>DIGITAL DEVELOPMENT</CapsHeadingLarge>
+            <CapsHeadingLarge
+              css={css`
+                color: ${colors.primary};
+              `}
+              w={800}
+            >
+              DIGITAL DEVELOPMENT
+            </CapsHeadingLarge>
             <LP>
               Full stack development
               <br /> Information Architecture
@@ -193,8 +198,205 @@ const IndexPage = () => {
               <br /> Android Applications
             </LP>
           </div>
-        </div>
+        </ServicesBlock>
       </OurApproachSection>
+      <WorkSection>
+        <H2
+          css={css`
+            color: ${colors.white};
+          `}
+        >
+          We work collaboratively with our clients and take pride in creating
+          delightful user experiences.
+        </H2>
+        {/* Subtitle Part */}
+        <AllWorkLink>
+          <H3
+            css={css`
+              color: rgba(255, 255, 255, 0.3);
+            `}
+          >
+            All work
+          </H3>
+          <Arrow
+            width={30}
+            height={35}
+            css={css`
+              path {
+                fill: rgba(255, 255, 255, 0.3);
+              }
+              margin-left: 1.7rem;
+            `}
+          />
+        </AllWorkLink>
+
+        <HeadingWithAccent
+          css={css`
+            position: absolute;
+            z-index: 1;
+            top: 15%;
+            right: 5%;
+            transform: translate(-5%, -15%);
+          `}
+        >
+          <H1
+            css={css`
+              color: rgba(255, 255, 255, 0.3);
+              transform: rotate(-90deg);
+            `}
+          >
+            Featured Work
+            <br /> + Case Studies
+          </H1>
+          <AccentExtraLarge
+            css={css`
+              transform: translate(-10rem, 30%);
+            `}
+          />
+        </HeadingWithAccent>
+        {/* Work Cards */}
+        <div
+          css={css`
+            display: flex;
+          `}
+        >
+          {/* First Column */}
+          <div>
+            <WorkCard
+              smallHeading="EARWIG - CONSTRUCTION"
+              bigHeading="Building a powerful reviews platform setting new standards for workers in the construction industry"
+              fileName="mask-group"
+              altText="mask group"
+            />
+            <WorkCard
+              smallHeading="EARWIG - CONSTRUCTION"
+              bigHeading="Building a powerful reviews platform setting new standards for workers in the construction industry"
+              fileName="mask-group"
+              altText="mask group"
+            />
+            <div
+              css={css`
+                margin-top: 7rem;
+              `}
+            >
+              <Button
+                title="LOVE WHAT YOU SEE? GET IT TOUCH"
+                hasArrow
+                capitalize
+                onClick={() => null}
+                backgroundColor={colors.accent}
+              />
+            </div>
+          </div>
+          {/* Second Column */}
+          <div
+            css={css`
+              margin-top: 25%;
+            `}
+          >
+            <WorkCard
+              smallHeading="EARWIG - CONSTRUCTION"
+              bigHeading="Building a powerful reviews platform setting new standards for workers in the construction industry"
+              fileName="mask-group"
+              altText="mask group"
+            />
+            <WorkCard
+              smallHeading="EARWIG - CONSTRUCTION"
+              bigHeading="Building a powerful reviews platform setting new standards for workers in the construction industry"
+              fileName="mask-group"
+              altText="mask group"
+            />
+          </div>
+        </div>
+      </WorkSection>
+      <TestimonialsSection>
+        <Quote
+          headline="In my experience, Yalla are a creative, reliable and trusted partner to work with."
+          person="LAURA JENNER"
+          position="DIGITAL PROJECT MANAGER"
+        >
+          <p>
+            From start to fruition, Joe was available every step of the way –
+            having time to chat through the development, and full transparency
+            of the build through GitHub gave me and my stakeholders great
+            confidence.
+          </p>
+          <br />
+          <p>
+            Yalla worked flexibly with our budget and timescale for the Tech for
+            Good project we’d asked them to help us with. Their strong team
+            management also helped us feel we were able to give feedback
+            quickly, and feel really involved in the process. As Joe and Simon
+            are also Founders and Coders grads, I also felt they had a strong
+            sense of product development from both a UX and technical point of
+            view.
+          </p>
+          <br />
+          <p>
+            Above all, Yalla held similar values to our organisation – an
+            interest in social impact projects, and driven by value for users. I
+            would not hesitate to work with them again.
+          </p>
+          <br />
+        </Quote>
+      </TestimonialsSection>
+      <StoriesSection>
+        <div>
+          {/* for the heading and the accent */}
+          <HeadingWithAccent
+            css={css`
+              z-index: 2;
+              transform: translate(75%, 0);
+            `}
+          >
+            <H1
+              css={css`
+                color: ${colors.black};
+                transform: rotate(-90deg);
+              `}
+            >
+              Stories
+            </H1>
+            <AccentLarge
+              css={css`
+                transform: translate(0, 50%);
+              `}
+            />
+          </HeadingWithAccent>
+        </div>
+        <div
+          css={css`
+            display: flex;
+            width: 120%;
+            margin-top: 4rem;
+
+            overflow: hidden;
+            div {
+              margin-right: 4rem;
+            }
+          `}
+        >
+          {/* for displaying the stories */}
+          <StoryCard
+            smallHeading="RESET - DIGITAL FOR GOOD"
+            bigHeading="Yalla Cooperative: Coding for Impact Between Europe and Gaza"
+            fileName="mask-group"
+            altText="mask group"
+          />
+          <StoryCard
+            smallHeading="RESET - DIGITAL FOR GOOD"
+            bigHeading="Yalla Cooperative: Coding for Impact Between Europe and Gaza"
+            fileName="mask-group"
+            altText="mask group"
+          />
+          <StoryCard
+            smallHeading="RESET - DIGITAL FOR GOOD"
+            bigHeading="Yalla Cooperative: Coding for Impact Between Europe and Gaza"
+            fileName="mask-group"
+            altText="mask group"
+          />
+        </div>
+      </StoriesSection>
     </Layout>
   )
 }
