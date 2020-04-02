18b395f5faba36221b14554665f859f42b73ff0b
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-04-03 00:26:23 +0300

    fix: paragraph console error

--- src/pages/index.js
+++ src/pages/index.js
@@ -315,28 +315,23 @@ const IndexPage = () => {
           person="LAURA JENNER"
           position="DIGITAL PROJECT MANAGER"
         >
-          <p>
-            From start to fruition, Joe was available every step of the way –
-            having time to chat through the development, and full transparency
-            of the build through GitHub gave me and my stakeholders great
-            confidence.
-          </p>
+          From start to fruition, Joe was available every step of the way –
+          having time to chat through the development, and full transparency of
+          the build through GitHub gave me and my stakeholders great confidence.
           <br />
-          <p>
-            Yalla worked flexibly with our budget and timescale for the Tech for
-            Good project we’d asked them to help us with. Their strong team
-            management also helped us feel we were able to give feedback
-            quickly, and feel really involved in the process. As Joe and Simon
-            are also Founders and Coders grads, I also felt they had a strong
-            sense of product development from both a UX and technical point of
-            view.
-          </p>
           <br />
-          <p>
-            Above all, Yalla held similar values to our organisation – an
-            interest in social impact projects, and driven by value for users. I
-            would not hesitate to work with them again.
-          </p>
+          Yalla worked flexibly with our budget and timescale for the Tech for
+          Good project we’d asked them to help us with. Their strong team
+          management also helped us feel we were able to give feedback quickly,
+          and feel really involved in the process. As Joe and Simon are also
+          Founders and Coders grads, I also felt they had a strong sense of
+          product development from both a UX and technical point of view.
+          <br />
+          <br />
+          Above all, Yalla held similar values to our organisation – an interest
+          in social impact projects, and driven by value for users. I would not
+          hesitate to work with them again.
+          <br />
           <br />
         </Quote>
       </TestimonialsSection>
