6ebbd982055100fb8508e51b4b336330849fdc4f
Fadi Omar <39485495+fadeomar@users.noreply.github.com>
2020-05-02 18:40:23 +0300

    our approach page (#64)
    * ci: add startegy section image
    
    * ci: add header image
    
    * ci: add design images
    
    * refactor: add style for the image component props
    
    * ci: add arrow down to svg file
    
    * ci: add style to our approach page
    
    * build: create our approach page
    
    * feat: add digital section image
    
    * feat: add digital section
    
    * feat: add style for digital section
    
    * refactor: edit the accent position
    
    * refactor: edit the title positon
    
    * refactor: edit the nav width and add link to logo
    
    * refactor: edit the propTypes in inage component
    
    * feat: add List instaed of p in ourapproach page
    
    * feat: add style to List component

--- src/components/layout/Header/Nav.js
+++ src/components/layout/Header/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Logo from '../../../../static/svg/yalla-logo.svg'
@@ -16,6 +17,12 @@ const Nav = styled.nav`
 const NavContainer = styled.div`
   position: absolute;
   top: 0;
+  width: 100%;
+`
+
+const Img = styled.img`
+  width: 6rem;
+  height: 6rem;
 `
 
 const Span = styled.span`
@@ -54,14 +61,16 @@ const LogoContainer = styled(MenuContainer)``
 const Navbar = ({ menuColor, logoColor }) => (
   <NavContainer>
     <Nav>
-      <LogoContainer type="button">
-        {logoColor === 'black' ? (
-          <img src={LogoBlack} alt="yalla-logo" />
-        ) : (
-          <img src={Logo} alt="yalla-logo" />
-        )}
-      </LogoContainer>
-      <MenuContainer type="button">
+      <Link to="/">
+        <LogoContainer>
+          {logoColor === 'black' ? (
+            <Img src={LogoBlack} alt="yalla-logo" />
+          ) : (
+            <Img src={Logo} alt="yalla-logo" />
+          )}
+        </LogoContainer>
+      </Link>
+      <MenuContainer>
         <Span menuColor={menuColor}>&nbsp;</Span>
       </MenuContainer>
     </Nav>
---
+++ src/styles/ourApproach.js
@@ -0,0 +1,158 @@
+import BackgroundImage from 'gatsby-background-image'
+import styled from 'styled-components'
+import { H2, CapsHeadingSmall } from '../components/elements'
+import { psize, lineHeight } from '../utils'
+
+export const ImageBackground = styled(BackgroundImage)`
+  background-image: url('/images/our-approach.png') no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+`
+
+export const List = styled.ul`
+  font-size: ${psize.l};
+  line-height: ${lineHeight.l};
+  font-weight: 400;
+  letter-spacing: 0.03em;
+  list-style: none;
+`
+// header style //
+
+export const Header = styled.header`
+  height: 100vh;
+`
+
+export const PageTitle = styled(H2)`
+  color: #fff;
+  text-align: center;
+`
+
+export const CapsHeadingSmallModifier = styled(CapsHeadingSmall)`
+  color: #fff;
+  position: absolute;
+  top: -5%;
+  left: 50%;
+  transform: translate(-50%, 5%);
+`
+
+export const TextWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 60%;
+  right: 0;
+  transform: translate(0, -50%);
+  padding: 1.5rem 0;
+  display: flex;
+  flex-direction: column;
+`
+export const ArrowImage = styled.img`
+  margin-top: 100px;
+`
+
+// introduction style //
+
+export const Introduction = styled.section`
+  padding: 14rem 12.5rem 14rem;
+  width: 70%;
+  display: flex;
+  h3 {
+    margin-bottom: 4.5rem;
+  }
+  p {
+    margin-bottom: 4rem;
+  }
+  small {
+    font-size: 100%;
+    font-weight: 700;
+  }
+`
+
+// strategy section style //
+
+export const StrategySection = styled.section`
+  margin-top: 12rem;
+`
+
+export const HeadingWithAccent = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: -120px;
+`
+
+export const StartagyContnet = styled.div`
+  background: rgba(247, 247, 247, 0.7);
+  display: flex;
+  margin-right: 21.1rem;
+  height: 550px;
+`
+export const StrategyBlock = styled.div`
+  flex: 2;
+  margin: 8rem 0 11rem 0;
+  div:not(:last-child) {
+    margin-bottom: 4.8rem;
+  }
+`
+export const StartagyImages = styled.div`
+  margin-top: 22rem;
+  display: flex;
+  justify-content: flex-end;
+  div {
+    margin-left: 30px;
+  }
+`
+
+// design section style //
+
+export const DesignSection = styled.section`
+  margin-top: 22rem;
+`
+export const DesignContent = styled.div`
+  background: rgba(247, 247, 247, 0.7);
+  display: flex;
+  flex-direction: row-reverse;
+  margin-left: 21.1rem;
+  height: 55rem;
+`
+export const HeadingWithAccentDesign = styled.div`
+  display: flex;
+  align-items: center;
+  transform: translateY(-5rem);
+  h1 {
+    margin-top: -12rem;
+    margin-right: 10rem;
+  }
+`
+
+export const Headings = styled.div``
+
+export const DesignBlock = styled.div`
+  flex: 2;
+  margin: 8rem 0 11rem 0;
+  padding: 0 10rem;
+
+  div:not(:last-child) {
+    margin-bottom: 4.8rem;
+  }
+`
+
+export const DesignImages = styled.div`
+  margin-top: 22rem;
+  display: flex;
+  justify-content: flex-start;
+  div {
+    margin-right: 30px;
+  }
+`
+
+// digital development section style //
+
+export const DigitalSection = styled.section`
+  margin-top: 28rem;
+`
+export const DigitalImages = styled.div`
+  margin: 22rem 0;
+  display: flex;
+  justify-content: flex-end;
+`
