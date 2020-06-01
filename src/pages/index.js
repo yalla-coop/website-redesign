5d38f953412c00dae1b838cbf8463cf84bdede23
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-06-01 22:56:03 +0300

    chore: add a slider for the quotes

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,8 @@ import NavBar from '../components/layout/Header/Nav'
 import ArrowDown from '../../static/svg/arrow-down.svg'
 import Arrow from '../../static/svg/arrow'
 
-import { WorkCard, Quote, StoryCard } from '../components/common'
+import { WorkCard, StoryCard } from '../components/common'
+import QuotesSlider from '../components/QuotesSlider'
 
 import { colors } from '../utils'
 
@@ -327,30 +328,7 @@ const IndexPage = () => {
         </div>
       </WorkSection>
       <TestimonialsSection>
-        <Quote
-          headline="In my experience, Yalla are a creative, reliable and trusted partner to work with."
-          person="LAURA JENNER"
-          position="DIGITAL PROJECT MANAGER"
-        >
-          From start to fruition, Joe was available every step of the way –
-          having time to chat through the development, and full transparency of
-          the build through GitHub gave me and my stakeholders great confidence.
-          <br />
-          <br />
-          Yalla worked flexibly with our budget and timescale for the Tech for
-          Good project we’d asked them to help us with. Their strong team
-          management also helped us feel we were able to give feedback quickly,
-          and feel really involved in the process. As Joe and Simon are also
-          Founders and Coders grads, I also felt they had a strong sense of
-          product development from both a UX and technical point of view.
-          <br />
-          <br />
-          Above all, Yalla held similar values to our organisation – an interest
-          in social impact projects, and driven by value for users. I would not
-          hesitate to work with them again.
-          <br />
-          <br />
-        </Quote>
+        <QuotesSlider />
       </TestimonialsSection>
       <StoriesSection>
         <div>
