3db034686f83fcdf164b1761507a723cd6c9a44d
Jemila Abulhawa <jemila.abulhawa@gmail.com>
2020-10-30 12:19:08 +0000

    style: menu, tablet and laptop

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -4,7 +4,7 @@ import { Link as GatsbyLink } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Cross from '../../static/svg/cross.svg'
-import { size, letterSpacing, colors, spacings } from '../utils'
+import { size, letterSpacing, colors, spacings, breakpoints } from '../utils'
 
 import { Wrapper as ContentWrapper } from '../components/contentWrapper'
 
@@ -16,8 +16,20 @@ const MenuWrapper = styled(ContentWrapper)`
   );
   color: white;
   height: 100vh;
-  padding-top: 16px;
-  padding-right: 16px;
+  padding-top: ${spacings[3]};
+  padding-right: ${spacings[3]};
+
+  ${breakpoints.laptop} {
+    padding-left: 0;
+    padding-right: 0;
+  }
+
+  ${breakpoints.laptopLarge} {
+    padding-top: ${spacings[4]};
+    padding-bottom: ${spacings[6]};
+    padding-left: 0;
+    padding-right: 0;
+  }
 `
 
 export const Link = styled(GatsbyLink)`
@@ -26,7 +38,16 @@ export const Link = styled(GatsbyLink)`
   letter-spacing: ${letterSpacing.small};
   font-weight: 700;
   text-decoration: none;
-  margin-bottom: 48px;
+  margin-bottom: ${spacings[6]};
+
+  ${breakpoints.tabletVertical} {
+    text-align: center;
+  }
+
+  ${breakpoints.laptopLarge} {
+    font-size: ${size.xl};
+    padding-bottom: ${spacings[6]};
+  }
 `
 
 export const CrossIcon = styled.img`
@@ -36,6 +57,16 @@ export const CrossIcon = styled.img`
 
   width: 48px;
   margin-bottom: ${spacings[7]};
+
+  ${breakpoints.laptop} {
+    margin-right: ${spacings[3]};
+  }
+
+  ${breakpoints.laptopLarge} {
+    width: 64px;
+    margin-right: ${spacings[4]};
+    margin-bottom: ${spacings[9]};
+  }
 `
 
 const MenuPage = () => (
