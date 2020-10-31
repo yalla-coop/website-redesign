bf8e2d371de6f2fbd228353b167e7f7afea3e9aa
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-10-31 17:12:40 +0200

    feat: add an image for SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -19,6 +19,7 @@ function SEO({ description, lang, meta, title }) {
             title
             description
             author
+            image
           }
         }
       }
@@ -42,6 +43,10 @@ function SEO({ description, lang, meta, title }) {
         {
           property: 'og:title',
           content: title,
+        },
+        {
+          property: 'og:image',
+          content: site.siteMetadata.image,
         },
         {
           property: 'og:description',
