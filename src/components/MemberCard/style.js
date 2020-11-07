b70689b7abb31a526c2450032072358bc016baf9
fadi <fadeomar2015@gmail.com>
2020-11-07 20:12:53 +0200

    add style for the membercard component

---
+++ src/components/MemberCard/style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
+
+import { CapsHeadingMedium } from '../elements'
+
+export const Header = styled.header`
+  height: 100vh;
+`
+
+export const CardWarapper = styled.div`
+  width: 336px;
+  @media (max-width: 600px) {
+    width: 272px;
+  }
+`
+
+export const ImageBackground = styled(BackgroundImage)`
+  background-size: cover;
+  height: 386px;
+  width: 100%;
+  position: relative;
+  margin-top: 0;
+  @media (max-width: 600px) {
+    width: 312px;
+  }
+`
+
+export const Name = styled(CapsHeadingMedium)`
+  padding-left: 13px;
+  font-family: Karu;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 26px;
+`
+
+export const Descrip = styled(CapsHeadingMedium)`
+  padding-left: 13px;
+  font-family: Karu;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 26px;
+  letter-spacing: 0.15em;
+  text-transform: uppercase;
+  color: #e0e0e0;
+`
