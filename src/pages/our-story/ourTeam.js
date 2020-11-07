60f8989201e7266508516e7e479ae47253038ee4
fadi <fadeomar2015@gmail.com>
2020-11-07 20:13:38 +0200

    add our team members data

---
+++ src/pages/our-story/ourTeam.js
@@ -0,0 +1,40 @@
+const ourTeam = [
+  {
+    key: 'joe',
+    image: null,
+    name: 'JOE FRIEL',
+    description: 'CO - FOUNDER / DESIGNER / FULL STACK DEVELOPER',
+  },
+  {
+    key: 'simon',
+    image: null,
+    name: 'SIMON DUPREE',
+    description: 'CO - FOUNDER / FULL STACK DEVELOPER',
+  },
+  {
+    key: 'ramy',
+    image: null,
+    name: 'RAMY AL SHURAFA',
+    description: 'LEAD FULL STACK DEVELOPER / QUALITY ASSURANCE',
+  },
+  {
+    key: 'jem',
+    image: null,
+    name: 'JEM ABULHAWA',
+    description: 'LEAD DESIGNER',
+  },
+  {
+    key: 'ahmed',
+    image: null,
+    name: 'AHMED',
+    description: 'FULL STACK DEVELOPER',
+  },
+  {
+    key: 'fadi',
+    image: null,
+    name: 'FADI',
+    description: 'FULL STACK DEVELOPER',
+  },
+]
+
+export default ourTeam
