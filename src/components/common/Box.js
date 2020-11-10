623430a90b075fde6b6159a6b6cfcfe9892bec63
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-11-10 23:35:49 +0200

    build: box component for future use

---
+++ src/components/common/Box.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Box = styled.div`
+  margin-top: ${({ marginTop }) => marginTop};
+  margin-bottom: ${({ marginBottom }) => marginBottom};
+  margin-left: ${({ marginLeft }) => marginLeft};
+  margin-right: ${({ marginRight }) => marginRight};
+  margin: ${({ margin }) => margin};
+  padding-top: ${({ paddingTop }) => paddingTop};
+  padding-bottom: ${({ paddingBottom }) => paddingBottom};
+  padding-left: ${({ paddingLeft }) => paddingLeft};
+  padding-right: ${({ paddingRight }) => paddingRight};
+  padding: ${({ padding }) => padding};
+`
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,5 @@
 export { default as Quote } from './Quote'
 export { default as StoryCard } from './StoryCard'
 export { default as WorkCard } from './WorkCard'
+export { default as MetaData } from './MetaData'
+export { Box } from './Box'
